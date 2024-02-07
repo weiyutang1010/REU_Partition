d58b51de3678bee33815a4b449473dfc1c8c4489
weiyutang1010 <weiyutang1010@outlook.com>
2024-02-06 19:50:55 -0800

    Modified figures

--- ncrna_design/sampling/graph.py
+++ ncrna_design/sampling/graph.py
@@ -6,22 +6,24 @@ if sys.argv[2] == 'samples':
         lines = file.read().split('\n')
         
         n = int(lines[0].split(', ')[0].split(': ')[1])
-        exact_value = float(lines[n+1].split(':  ')[1])
+        exact_value = float(lines[n+1].split(': ')[1])
+        jensen_value = float(lines[n+2].split(': ')[1])
 
         k = []
         val = []
-        for line in lines[n+2:n+2002]:
+        for line in lines[n+3:n+2003]:
             if len(line) > 0:
                 k.append(int(line.split()[0]))
                 val.append(float(line.split()[1]))
 
-        plt.axhline(y=exact_value, color='r', label='Exact Value')
+        plt.axhline(y=exact_value, color='r', label='Exact Value (Full model)')
+        plt.axhline(y=jensen_value, color='g', label='Jensen Approximation (Simple model)')
 
         plt.xlabel('Number of Samples')
-        plt.ylabel('E[log Q(x)]')
-        plt.title('Monte Carlo Sampling, n = 9, Uniform Distribution')
+        plt.ylabel('E[log Q(x)] (kcal/mol)')
+        plt.title(f'Monte Carlo Sampling, n = {n}, Uniform Distribution')
 
-        plt.plot(k, val, label='Approximated Value')
+        plt.plot(k, val, label='Sample Approximation (Full model)')
         plt.legend()
         plt.savefig(f'samples/{sys.argv[1]}.png', format='png', bbox_inches='tight')
 else:
