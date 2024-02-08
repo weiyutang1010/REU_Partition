41f484fae85782265db03540e51ec8dbe921f9f4
weiyutang1010 <weiyutang1010@outlook.com>
2024-02-07 22:10:38 -0800

    new graphs

--- ncrna_design/sampling/graph.py
+++ ncrna_design/sampling/graph.py
@@ -7,8 +7,7 @@ if sys.argv[2] == 'samples':
         
         n = int(lines[0].split(', ')[0].split(': ')[1])
         exact_value = float(lines[n+1].split(': ')[1])
-        jensen_value_simple = float(lines[n+2].split(': ')[1])
-        jensen_value_full = float(lines[n+3].split(': ')[1])
+        jensen_value_full = float(lines[n+2].split(': ')[1])
 
         k = []
         val = []
@@ -17,14 +16,14 @@ if sys.argv[2] == 'samples':
                 k.append(int(line.split()[0]))
                 val.append(float(line.split()[1]))
 
-        plt.ylim(exact_value-0.001, exact_value+0.005)
+        plt.ylim(exact_value-0.0002, exact_value+0.0002)
 
-        plt.axhline(y=exact_value, color='r', label='Exact Value (Full model)')
+        plt.axhline(y=exact_value, color='r', alpha=1.0, label='Exact Value (Full model)')
         # plt.axhline(y=jensen_value_simple, color='orange', label='Jensen Approximation (Simple model)')
-        plt.axhline(y=jensen_value_full, color='g', label='Jensen Approximation (Full model)')
+        plt.axhline(y=jensen_value_full, color='g', alpha=0.6, label='Jensen Approximation (Full model)')
 
         plt.xlabel('Number of Samples')
-        plt.ylabel('Free Energy of Ensemble (kcal/mol)')
+        plt.ylabel('Expected Free Energy of Ensemble (kcal/mol)')
         plt.title(f'Monte Carlo Sampling, n = {n}, Uniform Distribution')
 
         plt.plot(k, val, label='Sample Approximation (Full model)')
@@ -41,24 +40,25 @@ else:
         exact = []
         n = int(lines[0].split(', ')[0].split(': ')[1])
         k = int(lines[0].split(', ')[1].split(': ')[1])
-        for line in lines[2:200]:
+        for line in lines[2:]:
             if len(line) > 0:
                 entropy.append(float(line.split(", ")[0]))
                 sampling.append(float(line.split(", ")[1]))
-                if len(line.split(", ")) > 2:
-                    jensen.append(float(line.split(", ")[2]))
-                    exact.append(float(line.split(", ")[3]))
+                jensen.append(float(line.split(", ")[2]))
+                exact.append(float(line.split(", ")[3]))
 
         sampling_gap = [x - y for x, y in zip(sampling, exact)]
         jensen_gap = [x - y for x, y in zip(jensen, exact)]
+
+        plt.xlim(-0.0, 2.0)
+        # plt.ylim(-0.025, 0.01)
 
         plt.xlabel('Avg Positional Entropy')
         plt.ylabel('Approximation Gap (Approx - Exact)')
         plt.title(f'Approximation Gap vs. Avg Positional Entropy, n = {n}')
 
         plt.axhline(y=0.0, color='r')
-        plt.scatter(entropy, sampling_gap, label=f'Sampling k = {k}', marker='x', alpha=0.6)
-        if len(jensen) > 0:
-            plt.scatter(entropy, jensen_gap, label='Jensen\'s Approx', marker='x', alpha=0.6)
+        plt.scatter(entropy, sampling_gap, label=f'Sampling k = {k}', marker='x', alpha=0.5)
+        plt.scatter(entropy, jensen_gap, label='Jensen\'s Approx', marker='x', alpha=0.5)
         plt.legend()
         plt.savefig(f'approx_gap/{sys.argv[1]}.png', format='png', bbox_inches='tight')
