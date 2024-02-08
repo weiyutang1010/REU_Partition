37415cd7a2a7e1a5353541dc4fa79279513b67bc
Wei Tang <tangwe@mozart.eecs.oregonstate.edu>
2024-02-07 19:13:26 -0800

    Second Order

--- ncrna_design/sampling/sampling.py
+++ ncrna_design/sampling/sampling.py
@@ -5,7 +5,7 @@ import numpy as np
 import RNA
 import concurrent.futures
 
-sequences = {}
+sequences = []
 log_Q_cached = {}
 
 def generate_sequences(*args, n):
@@ -70,20 +70,6 @@ def probability(D, x):
         p *= D[idx][nuc_to_idx[c]]
     return p
 
-
-def E_log_Q(D):
-    """Brute Force"""
-    n = len(D)
-
-    if n not in sequences:
-        sequences[n] = generate_sequences('ACGU', n=n)
-
-    value = 0.
-    for seq in sequences[n]:
-        value += probability(D, seq) * log_Q_cached["".join(seq)]
-
-    return value
-
 def compare(n, k):
     # Given a distribution of length n
     D = generate_distribution(n) # random distribution
@@ -125,19 +111,92 @@ def approximation_gap(n, k, t):
         # Define tasks to be executed concurrently
         futures = [executor.submit(compare, n, k) for _ in range(t)]
         concurrent.futures.wait(futures)
-    
+
+def E_log_Q(D):
+    """Brute Force"""
+    n = len(D)
+
+    value = 0.
+    for seq in sequences:
+        value += probability(D, seq) * log_Q_cached[seq]
+
+    return value
+
+kT = 61.63207755
+def log_E_Q(D):
+    """Brute Force"""
+    n = len(D)
+
+    value = 0.
+    for seq in sequences:
+        value += probability(D, seq) * np.exp(log_Q_cached[seq] * 100.0 / -kT)
+
+    return np.log(value) * -kT / 100.0, value
+
+# def jensen_approx(D):
+#     # D = np.array([[.25,.25,.25,.25] for _ in range(n)]) # random distribution
+#     # print(D)
+
+#     with concurrent.futures.ThreadPoolExecutor(max_workers=12) as executor:
+#         futures = [executor.submit(log_Q, "".join(seq)) for seq in sequences[n]]
+#         concurrent.futures.wait(futures)
+#     partition_jensen = log_E_Q(D)
+#     print("Jensen value: ", partition_jensen)
+
+def second_order_approx(D, E_Qx):
+    """Brute Force, E_Qx = E[Q(x)]"""
+    n = len(D)
+
+    E_Qx_squared = 0. # E[Q(x)^2]
+    for seq in sequences:
+        Q_x = np.exp(log_Q_cached[seq] * 100.0 / -kT)
+        E_Qx_squared += probability(D, seq) * (Q_x * Q_x)
+
+    var_Qx = E_Qx_squared - (E_Qx * E_Qx)
+    second_order = np.log(E_Qx) - (var_Qx / (2 * E_Qx * E_Qx))
+
+    return second_order * -kT / 100.0
+
+# def second_order_approx(n):
+#     D = np.array([[.25,.25,.25,.25] for _ in range(n)]) # random distribution
+#     print(D)
+
+#     sequences[n] = generate_sequences('ACGU', n=n)
+#     with concurrent.futures.ThreadPoolExecutor(max_workers=12) as executor:
+#         futures = [executor.submit(log_Q, "".join(seq)) for seq in sequences[n]]
+#         concurrent.futures.wait(futures)
+#     partition_second_order = V_Q(D)
+#     print("Second Order value: ", partition_second_order)
+
+def read_sequences(n):
+    with open(f'Qx/n{n}.txt', 'r') as file:
+        lines = file.read().split('\n')
+        for line in lines[:-1]:
+            seq = line.split(' ')[0]
+            sequences.append(seq)
+            log_Q_cached[seq] = float(line.split(' ')[1])
+
 def sampling_test(n, k):
     # Given a distribution of length n
-    # D = np.random.dirichlet(np.ones(4), size=n) # random distribution
     D = np.array([[.25,.25,.25,.25] for _ in range(n)]) # random distribution
     print(D)
 
-    sequences[n] = generate_sequences('ACGU', n=n)
-    with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
-        futures = [executor.submit(log_Q, "".join(seq)) for seq in sequences[n]]
-        concurrent.futures.wait(futures)
+    # sequences[n] = generate_sequences('ACGU', n=n)
+    # with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
+    #     futures = [executor.submit(log_Q, "".join(seq)) for seq in sequences[n]]
+    #     concurrent.futures.wait(futures)
+
+    sequences.clear()
+    log_Q_cached.clear()
+    read_sequences(n)
+
+    partition_jensen, E_Qx = log_E_Q(D)
     partition_exact = E_log_Q(D)
-    print("Exact value: ", partition_exact)
+    partition_second = second_order_approx(D, E_Qx)
+
+    print("Exact value (full): ", partition_exact)
+    print("Jensen value (full): ", partition_jensen)
+    print("Second Order Approximation (full): ", partition_second)
 
     samples = []
     for x in D:
@@ -150,67 +209,9 @@ def sampling_test(n, k):
         curr_sum += log_Q_cached[seq]
         print(i+1, curr_sum / (i + 1))
 
-kT = 61.63207755
-def log_E_Q(D):
-    """Brute Force"""
-    n = len(D)
-
-    if n not in sequences:
-        sequences[n] = generate_sequences('ACGU', n=n)
-
-    value = 0.
-    for seq in sequences[n]:
-        value += probability(D, seq) * np.exp(log_Q_cached["".join(seq)] * 100 / -kT)
-
-    return np.log(value) * -kT / 100.0
-
-def jensen_approx(n):
-    D = np.array([[.25,.25,.25,.25] for _ in range(n)]) # random distribution
-    print(D)
-
-    sequences[n] = generate_sequences('ACGU', n=n)
-    with concurrent.futures.ThreadPoolExecutor(max_workers=12) as executor:
-        futures = [executor.submit(log_Q, "".join(seq)) for seq in sequences[n]]
-        concurrent.futures.wait(futures)
-    partition_jensen = log_E_Q(D)
-    print("Jensen value: ", partition_jensen)
-
-def V_Q(D):
-    """Brute Force"""
-    n = len(D)
-
-    if n not in sequences:
-        sequences[n] = generate_sequences('ACGU', n=n)
-
-    value = 0. # E[Q(x)]
-    value_squared = 0. # E[Q(x)^2]
-    for seq in sequences[n]:
-        Q_x = np.exp(log_Q_cached["".join(seq)] * 100 / -kT)
-        value += probability(D, seq) * Q_x
-        value_squared += probability(D, seq) * (Q_x * Q_x)
-
-    var_Qx = value_squared - (value * value)
-    var_Qx / (2 * value * value)
-
-    return np.log(value) * -kT / 100.0
-
-def second_order_approx(n):
-    D = np.array([[.25,.25,.25,.25] for _ in range(n)]) # random distribution
-    print(D)
-
-    sequences[n] = generate_sequences('ACGU', n=n)
-    with concurrent.futures.ThreadPoolExecutor(max_workers=12) as executor:
-        futures = [executor.submit(log_Q, "".join(seq)) for seq in sequences[n]]
-        concurrent.futures.wait(futures)
-    partition_second_order = V_Q(D)
-    print("Second Order value: ", partition_second_order)
-    
 if __name__ == '__main__':
     np.random.seed(seed=42)
     parser = argparse.ArgumentParser()
-
-    jensen_approx(7)
-    exit(0)
 
     # parser.add_argument("--id", type=int, default=0)
     # parser.add_argument("--lr", type=float, default=0.001)
@@ -225,15 +226,19 @@ if __name__ == '__main__':
     # parser.add_argument("--threads", type=int, default=8)
     # parser.add_argument("--init", type=str, default='uniform')
 
-    parser.add_argument("--mode", type=int, default=1)
-    parser.add_argument("--n", type=int, default=9)
-    parser.add_argument("--k", type=int, default=2000)
+    parser.add_argument("--mode", type=int, default=0)
+    parser.add_argument("--n", type=int, default=5)
+    parser.add_argument("--k", type=int, default=1000)
     parser.add_argument("--t", type=int, default=500)
     args = parser.parse_args()
 
-    print(f"n: {args.n}, k: {args.k}, t: {args.t}")
 
     if args.mode == 1:
+        print(f"n: {args.n}, k: {args.k}, t: {args.t}")
         approximation_gap(args.n, args.k, args.t)
     else:
-        sampling_test(args.n, args.k)
+        for n in range(5, 8):
+            with open(f'samples/n{n}.txt', 'w') as f:
+                sys.stdout = f
+                print(f"n: {n}, k: {args.k}")
+                sampling_test(n, args.k)
