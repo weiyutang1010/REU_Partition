9db6d99f3746dc9228bef2df41c8ef93debf5e48
Wei Tang <tangwe@jsbach.eecs.oregonstate.edu>
2024-02-07 23:01:51 -0800

    new results

--- ncrna_design/sampling/Qx.py
+++ ncrna_design/sampling/Qx.py
@@ -8,10 +8,21 @@ import concurrent.futures
 
 file_lock = threading.Lock()
 
-def log_Q(x, file):
+def valid(x, y):
+    _allowed_pairs = {'CG', 'GC', 'AU', 'UA', 'GU', 'UG'}
+    # for i, j in [(0, 10), (1, 9), (2, 8), (3, 7)]:
+    for i, j in [(0, 8), (1, 7), (2, 6)]:
+        if x[i] + x[j] not in _allowed_pairs:
+            return False
+    return True
+
+def log_Q(x, y, file):
+    if not valid(x, y):
+        return
+
     fc = RNA.fold_compound(x)
     with file_lock:
-        file.write(f"{x} {fc.pf()[1]:.100f}\n")
+        file.write(f"{x} {fc.pf()[1]:.30f}\n")
 
 def generate_sequences(*args, n):
     pools = [tuple(pool) for pool in args] * n
@@ -20,10 +31,21 @@ def generate_sequences(*args, n):
         result = [x+[y] for x in result for y in pool]
     return result
 
-for n in range(9, 13):
-    with open(f'Qx/n{n}.txt', 'w') as file:
-        sequences = generate_sequences('ACGU', n=n)
-        with concurrent.futures.ThreadPoolExecutor(max_workers=32) as executor:
-            futures = [executor.submit(log_Q, "".join(seq), file) for seq in sequences]
-            concurrent.futures.wait(futures)
+# for n in range(9, 13):
+#     with open(f'Qx/n{n}.txt', 'w') as file:
+#         sequences = generate_sequences('ACGU', n=n)
+#         with concurrent.futures.ThreadPoolExecutor(max_workers=32) as executor:
+#             futures = [executor.submit(log_Q, "".join(seq), file) for seq in sequences]
+#             concurrent.futures.wait(futures)
+
+n = 9
+y = "(((...)))"
+
+with open(f'Qx/n{n}_y.txt', 'w') as file:
+    sequences = generate_sequences('ACGU', n=n)
+    print("Generation Done")
+    with concurrent.futures.ThreadPoolExecutor(max_workers=32) as executor:
+        futures = [executor.submit(log_Q, "".join(seq), y, file) for seq in sequences]
+        concurrent.futures.wait(futures)
+
 
--- ncrna_design/sampling/sampling_coupled.py
+++ ncrna_design/sampling/sampling_coupled.py
@@ -15,60 +15,18 @@ def generate_sequences(*args, n):
         result = [x+[y] for x in result for y in pool]
     return result
 
-def generate_distribution(n):
-    test_distribution = []
-
-    rand_int = np.random.randint(2, 5)
-
-    for _ in range(n):
-
-        if rand_int == 2:
-            rand = np.concatenate((np.random.sample(2), np.array([1., 0.])), axis=0)
-            rand.sort()
-
-            test_distribution.append([
-                rand[1] - rand[0],
-                rand[2] - rand[1],
-                0.,
-                0.,
-            ])
-            np.random.shuffle(test_distribution[-1])
-
-        if rand_int == 3:
-            rand = np.concatenate((np.random.sample(2), np.array([1., 0.])), axis=0)
-            rand.sort()
-
-            test_distribution.append([
-                rand[1] - rand[0],
-                rand[2] - rand[1],
-                rand[3] - rand[2],
-                0.,
-            ])
-            np.random.shuffle(test_distribution[-1])
-
-        if rand_int == 4:
-            rand = np.concatenate((np.random.sample(3), np.array([1., 0.])), axis=0)
-            rand.sort()
-
-            test_distribution.append([
-                rand[1] - rand[0],
-                rand[2] - rand[1],
-                rand[3] - rand[2],
-                rand[4] - rand[3]
-            ])
-
-    return np.array(test_distribution)
-
 def log_Q(x):
     fc = RNA.fold_compound(x)
     log_Q_cached[x] = fc.pf()[1]
 
 def probability(D, x):
-    nuc_to_idx = {'A': 0, 'C': 1, 'G': 2, 'U': 3}
-    p = 1.
-    for idx, c in enumerate(x):
-        p *= D[idx][nuc_to_idx[c]]
-    return p
+    # nuc_to_idx = {'A': 0, 'C': 1, 'G': 2, 'U': 3}
+    # p = 1.
+    # for idx, c in enumerate(x):
+    #     p *= D[idx][nuc_to_idx[c]]
+    # return p
+    # return ((1/6) ** 4) * (.25 ** 4) # change
+    return ((1/6) ** 3) * (.25 ** 3) # change
 
 
 
@@ -134,17 +92,18 @@ def read_sequences(n):
     global log_Q_cached
     log_Q_cached = {}
 
-    with open(f'Qx/n{n}.txt', 'r') as file:
+    with open(f'Qx/n{n}_y.txt', 'r') as file:
         lines = file.read().split('\n')
         for line in lines[:-1]:
             seq = line.split(' ')[0]
             sequences.append(seq)
             log_Q_cached[seq] = float(line.split(' ')[1])
 
-def sampling_test(n, k):
+def sampling_test(n, k, y):
     # Given a distribution of length n
-    D = np.array([[.25,.25,.25,.25] for _ in range(n)]) # random distribution
-    print(D)
+    # D = np.array([[.25,.25,.25,.25] for _ in range(n)]) # random distribution
+    # D = np.array([[1/6,1/6,1/6] for _ in range(n)]) # random distribution
+    # print(D)
 
     # sequences[n] = generate_sequences('ACGU', n=n)
     # with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
@@ -153,20 +112,43 @@ def sampling_test(n, k):
 
     read_sequences(n)
 
+    D = [0.] * n
     partition_jensen, E_Qx = log_E_Q(D)
     partition_exact = E_log_Q(D)
-    partition_second = second_order_approx(D, E_Qx)
+    # partition_second = second_order_approx(D, E_Qx)
 
     print("Exact value (full): ", partition_exact)
     print("Jensen value (full): ", partition_jensen)
-    print("Second Order Approximation (full): ", partition_second)
+    print("Second Order Approximation (full): ", 0.)
 
-    samples = [np.random.choice(['A','C','G','U'], k, p=x) for x in D]
-    samples = np.array(samples).transpose()
-    seqs = ["".join(sample) for sample in samples]
+    stack = []
+    paired, unpaired = [], []
+    for j, c in enumerate(y):
+        if c == '(':
+            stack.append(j)
+        elif c == ')':
+            i = stack.pop()
+            paired.append((i, j))
+        else:
+            unpaired.append(j)
+
+    samples = []
+    for _ in range(k):
+        seq = ['A'] * n
+        for j in unpaired:
+            seq[j] = np.random.choice(['A','C','G','U'], p=[.25, .25, .25, .25])
+        for i, j in paired:
+            chosen = np.random.choice(['CG','GC','AU','UA', 'GU', 'UG'], p=[1/6,1/6,1/6,1/6,1/6,1/6])
+            seq[i] = chosen[0]
+            seq[j] = chosen[1]
+        samples.append("".join(seq))
+
+    # samples = [np.random.choice(['A','C','G','U'], k, p=x) for x in D]
+    # samples = np.array(samples).transpose()
+    # seqs = ["".join(sample) for sample in samples]
 
     curr_sum = 0.
-    for i, seq in enumerate(seqs):
+    for i, seq in enumerate(samples):
         curr_sum += log_Q_cached[seq]
         print(i+1, curr_sum / (i + 1))
 
@@ -188,12 +170,15 @@ if __name__ == '__main__':
     # parser.add_argument("--init", type=str, default='uniform')
 
     parser.add_argument("--mode", type=int, default=0)
-    parser.add_argument("--n", type=int, default=5)
+    parser.add_argument("--n", type=int, default=9)
     parser.add_argument("--k", type=int, default=2000)
     args = parser.parse_args()
 
-    for n in range(8, 10):
-        with open(f'samples/n{n}.txt', 'w') as f:
-            sys.stdout = f
-            print(f"n: {n}, k: {args.k}")
-            sampling_test(n, args.k)
+    # change
+    # y = "((((...))))."
+    y = "(((...)))"
+    n = len(y)
+    with open(f'samples_y/n{n}.txt', 'w') as f:
+        sys.stdout = f
+        print(f"n: {n}, k: {args.k}, y: {y}")
+        sampling_test(n, args.k, y)
